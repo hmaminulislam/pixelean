6258f947929d737c1df7089c0ea87129647ea02c
Aminul <mdaminulislamsahin14@gmail.com>
2023-03-10 16:13:01 +0600

    header, sidbar and ship report

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function showLeftDropdown (dropdown, btn) {
+    document
+      .getElementsByClassName(dropdown)[0]
+      .classList.toggle("hidden");
+    document
+      .getElementsByClassName(btn)[0]
+      .classList.toggle("left-drop-btn-active");
+}
