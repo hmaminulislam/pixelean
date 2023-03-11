2cea460141f7e1d5b3876e9d0c748367d0efced9
Aminul <mdaminulislamsahin14@gmail.com>
2023-03-11 18:58:50 +0600

    project done

--- js/main.js
+++ js/main.js
@@ -1,25 +1,23 @@
 const btnAll = document.querySelectorAll(".left-dropdown");
 
-  btnAll.forEach((btn) => {
-    btn.addEventListener("click", () => {
-
-      if (btn.classList.contains("activeDrop")) {
+btnAll.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    if (btn.classList.contains("activeDrop")) {
+      btn.classList.remove("activeDrop");
+    } else {
+      btnAll.forEach((btn) => {
         btn.classList.remove("activeDrop");
-      } else {
-        btnAll.forEach((btn) => {
-          btn.classList.remove("activeDrop");
-        });
-        btn.classList.toggle("activeDrop");
-      }
-
-    });
+      });
+      btn.classList.toggle("activeDrop");
+    }
   });
+});
 
-  // Right Side Navigation dropdown
+// Right Side Navigation dropdown
 
-  function showDropMenu() {
-    document.getElementById("nav-dropdown").classList.toggle("hidden");
-  }
+function showDropMenu() {
+  document.getElementById("nav-dropdown").classList.toggle("hidden");
+}
 
 document.addEventListener("click", () => {
   const btn = document.getElementById("right-drop-btn");
@@ -28,3 +26,19 @@ document.addEventListener("click", () => {
     document.getElementById("nav-dropdown").classList.add("hidden");
   }
 });
+
+// Left Sidebar Menu show
+
+const element = document.getElementsByClassName("main-left-side")[0];
+const icon = document.getElementById("mobile-sidebar-icon");
+
+function showLeftBar() {
+  element.classList.toggle("block");
+  icon.classList.toggle("rotate-icon");
+}
+
+// Left Sidebar Menu close
+function closeLeftbar() {
+  element.classList.remove("block");
+  icon.classList.remove("rotate-icon");
+}
