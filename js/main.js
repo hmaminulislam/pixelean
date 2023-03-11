ae51a31d1981fc4f1c923b0db1771d75485532bd
Aminul <mdaminulislamsahin14@gmail.com>
2023-03-11 10:47:41 +0600

    Navigation dropdown added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,30 @@
-function showLeftDropdown (dropdown, btn) {
-    document.getElementsByClassName(dropdown)[0].classList.toggle("hidden");
-    document
-      .getElementsByClassName(btn)[0]
-      .classList.toggle("left-drop-btn-active");
-}
+const btnAll = document.querySelectorAll(".left-dropdown");
+
+  btnAll.forEach((btn) => {
+    btn.addEventListener("click", () => {
+
+      if (btn.classList.contains("activeDrop")) {
+        btn.classList.remove("activeDrop");
+      } else {
+        btnAll.forEach((btn) => {
+          btn.classList.remove("activeDrop");
+        });
+        btn.classList.toggle("activeDrop");
+      }
+
+    });
+  });
+
+  // Right Side Navigation dropdown
+
+  function showDropMenu() {
+    document.getElementById("nav-dropdown").classList.toggle("hidden");
+  }
+
+document.addEventListener("click", () => {
+  const btn = document.getElementById("right-drop-btn");
+
+  if (!btn.contains(event.target)) {
+    document.getElementById("nav-dropdown").classList.add("hidden");
+  }
+});
