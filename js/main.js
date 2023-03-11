c653c76fc4f996c9151fd3766f4b44f34e575777
Aminul <mdaminulislamsahin14@gmail.com>
2023-03-11 07:31:36 +0600

    desktop ui completed

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 function showLeftDropdown (dropdown, btn) {
-    document
-      .getElementsByClassName(dropdown)[0]
-      .classList.toggle("hidden");
+    document.getElementsByClassName(dropdown)[0].classList.toggle("hidden");
     document
       .getElementsByClassName(btn)[0]
       .classList.toggle("left-drop-btn-active");
